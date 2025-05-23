666367b83a834c100e60fa894a72087b92070aeb
duybuihoang <91789217+duybuihoang@users.noreply.github.com>
2025-05-23 22:08:31 +0700

    chore: remove unnecessary import

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.SceneManagement;
 
 public enum GameMode
